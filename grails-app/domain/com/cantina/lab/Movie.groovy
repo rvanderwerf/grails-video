e8b4a5569b3ef5b9df9fe78a46f5e473dbcf8cc6
Burt Beckwith <beckwithb@vmware.com>
2012-08-30 03:14:19 -0400

    cleanup

--- grails-app/domain/com/cantina/lab/Movie.groovy
+++ grails-app/domain/com/cantina/lab/Movie.groovy
@@ -1,110 +1,87 @@
-
-/* Copyright 2006-2007 the original author or authors.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*
-* For more information please visit www.cantinaconsulting.com
-* or email info@cantinaconsulting.com
-*/
+/* Copyright 2006-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information please visit www.cantinaconsulting.com
+ * or email info@cantinaconsulting.com
+ */
 package com.cantina.lab
-import org.codehaus.groovy.grails.commons.ConfigurationHolder;
 
 /**
- * A domain class that represents a video for Grails.
+ * Represents a video.
  *
  * @author Matt Chisholm
  * @author Adam Stachelek
  */
 class Movie {
 
-    static transients = ["theFile"]
-    // static hasMany = []
-    // static belongsTo = []
-    static mapping = {
-    version: false
-    key column:'video_key'
-  }
+	static transients = ["theFile"]
 
-    public static final String STATUS_BLANK = "blank"
-    public static final String STATUS_REMOVED = "removed"
-    public static final String STATUS_NEW = "new"
-    public static final String STATUS_INPROGRESS = "inprogress"
-    public static final String STATUS_CONVERTED = "converted"
-    public static final String STATUS_FAILED = "failed"
+	static mapping = {
+		version false
+		key column: 'video_key'
+	}
 
-    
+	static final String STATUS_BLANK = "blank"
+	static final String STATUS_REMOVED = "removed"
+	static final String STATUS_NEW = "new"
+	static final String STATUS_INPROGRESS = "inprogress"
+	static final String STATUS_CONVERTED = "converted"
+	static final String STATUS_FAILED = "failed"
 
-    String url;                 //url to the content source
-    String title;               //title of the content
-    String fileName;            //name of the file
-    String description;         //description of the content or abstract. should be short
-    byte[] theFile;             //binary data for the file
-    Long size;                  //size of content in bytes
-    Date createDate;            //date the content was created
-    String createdBy;           //the name of the person who created the content
-    String pathFlv;             //path to the flv file
-    String pathThumb;           //the path to the thumb file - jpg
-    String pathMaster;          //path to the digital master movie - i.e, mpeg, mov, etc...
-    String contentType;          //the content type of the movie - usually flv
-    String contentTypeMaster;   //the content type of the digital master movie
-    Long playTime;              //duration of the movie in number of seconds
+	String url                 //url to the content source
+	String title               //title of the content
+	String fileName            //name of the file
+	String description         //description of the content or abstract. should be short
+	byte[] theFile             //binary data for the file
+	Long size                  //size of content in bytes
+	Date createDate            //date the content was created
+	String createdBy           //the name of the person who created the content
+	String pathFlv             //path to the flv file
+	String pathThumb           //the path to the thumb file - jpg
+	String pathMaster          //path to the digital master movie - i.e, mpeg, mov, etc...
+	String contentType          //the content type of the movie - usually flv
+	String contentTypeMaster   //the content type of the digital master movie
+	Long playTime              //duration of the movie in number of seconds
 
-    //unique key for the filenames on the file system
-    String key = new UUID(System.currentTimeMillis(),
-               System.currentTimeMillis()*System.currentTimeMillis()).toString();
+	//unique key for the filenames on the file system
+	String key = new UUID(System.currentTimeMillis(),
+		System.currentTimeMillis()*System.currentTimeMillis()).toString()
 
-    //status of the movie for the converstion process
-    String status = STATUS_BLANK;
+	//status of the movie for the converstion process
+	String status = STATUS_BLANK
 
-     def mvals = ConfigurationHolder.config.video
+	static constraints = {
+		url(nullable:true)
+		title(nullable:true)
+		fileName(nullable:true)
+		description(nullable:true)
+		contentType(nullable:true)
+		contentTypeMaster(nullable:true)
+		theFile(nullable:true)
+		pathFlv(nullable:true)
+		pathMaster(nullable:true)
+		pathThumb(nullable:true)
+		size(nullable:true)
+		createDate(nullable:true)
+		createdBy(nullable:true)
+		playTime(nullable:true)
+	}
 
-    static constraints = {
-
-        url(nullable:true)
-        title(nullable:true)
-        fileName(nullable:true)
-        description(nullable:true)
-        contentType(nullable:true)
-        contentTypeMaster(nullable:true)
-        theFile(nullable:true)
-        pathFlv(nullable:true)
-        pathMaster(nullable:true)
-        pathThumb(nullable:true)
-        size(nullable:true)
-        createDate(nullable:true)
-        createdBy(nullable:true)
-        playTime(nullable:true)
-
-    }
-
-
-    Movie(){}
-
-
-    Movie(File masterFile){
-
-        this.newFile(newFile);
-    }
-
-
-    def newFile(File masterFile) {
-
-        //get the submitted file
-        this.pathMaster = masterFile.canonicalPath
-        this.size = masterFile.length()
-        this.status = STATUS_NEW
-
-    }
-
-
+	void newFile(File masterFile) {
+		//get the submitted file
+		pathMaster = masterFile.canonicalPath
+		size = masterFile.length()
+		status = STATUS_NEW
+	}
 }
--- grails-app/services/com/cantina/lab/VideoService.groovy
+++ grails-app/services/com/cantina/lab/VideoService.groovy
@@ -1,257 +1,233 @@
+/* Copyright 2006-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information please visit www.cantinaconsulting.com
+ * or email info@cantinaconsulting.com
+ */
 package com.cantina.lab
-/* Copyright 2006-2007 the original author or authors.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*
-* For more information please visit www.cantinaconsulting.com
-* or email info@cantinaconsulting.com
-*/
 
-import org.codehaus.groovy.grails.commons.ConfigurationHolder;
 import org.springframework.beans.factory.InitializingBean
-import com.cantina.lab.Movie
+import org.springframework.transaction.annotation.Transactional
 
 /**
- * A service that manages video assets for Grails.
+ * Manages video assets.
  *
  * @author Matt Chisholm
  * @author Adam Stachelek
  */
-
 class VideoService implements InitializingBean {
 
-    boolean transactional = false
+	static transactional = false
 
-    def mvals = ConfigurationHolder.config.video
+	def grailsApplication
 
-    // Todo: research video capture from flash ie, red5
+	private mvals
 
-    void afterPropertiesSet() {
+	// Todo: research video capture from flash ie, red5
 
-        // Setup local asset path
-        buildLocalPath()
-    }
+	void afterPropertiesSet() {
+		buildLocalPath()
+		mvals = grailsApplication.config.video
+	}
 
-    def buildLocalPath() {
-        def f = new File(ConfigurationHolder.config.video.location)
+	void buildLocalPath() {
+		def f = new File(grailsApplication.config.video.location)
+		if (!f.exists()) {
+			f.mkdirs()
+		}
+	}
 
-        if (!f.exists()) {
-            f.mkdir()
-        }
-    }
+	boolean performConversion(File sourceVideo, File targetVideo, File thumb) {
 
-    def performConversion(File sourceVideo, File targetVideo, File thumb) {
+		String convertedMovieFileExtension = mvals.ffmpeg.fileExtension
+		boolean success = false
 
-        String convertedMovieFileExtension = mvals.ffmpeg.fileExtension;
-        def success = false;
+		if (convertedMovieFileExtension == "flv") {
+			String uniqueId = new UUID(System.currentTimeMillis(),
+				System.currentTimeMillis() * System.currentTimeMillis()).toString()
 
-        if (convertedMovieFileExtension == "flv") {
-            //create a unique id for the temp file
-            def uniqueId = new UUID(System.currentTimeMillis(),
-                    System.currentTimeMillis() * System.currentTimeMillis()).toString();
+			String tmpfile = mvals.location + uniqueId + "." + convertedMovieFileExtension
 
+			File tmp = new File(tmpfile) //temp file for contents during conversion
 
-            def tmpfile = mvals.location + uniqueId + "." + convertedMovieFileExtension;
+			String convertCmd = "${mvals.ffmpeg.path} -i ${sourceVideo.absolutePath} ${mvals.ffmpeg.conversionArgs} ${tmp.absolutePath}"
+			String metadataCmd = "${mvals.yamdi.path} -i ${tmp.absolutePath} -o ${targetVideo.absolutePath} -l"
+			String thumbCmd = "${mvals.ffmpeg.path} -i ${targetVideo.absolutePath} ${mvals.ffmpeg.makethumb} ${thumb.absolutePath}"
 
-            File tmp = new File(tmpfile) //temp file for contents during conversion
+			success = exec(convertCmd) //kick off the command to convert movie to flv
 
+			if (success) success = exec(metadataCmd) //kick off the command to add the metadata
 
-            String convertCmd = "${mvals.ffmpeg.path} -i ${sourceVideo.absolutePath} ${mvals.ffmpeg.conversionArgs} ${tmp.absolutePath}"
-            String metadataCmd = "${mvals.yamdi.path} -i ${tmp.absolutePath} -o ${targetVideo.absolutePath} -l"
-            String thumbCmd = "${mvals.ffmpeg.path} -i ${targetVideo.absolutePath} ${mvals.ffmpeg.makethumb} ${thumb.absolutePath}"
+			if (success) success = exec(thumbCmd) //kick off the command to create the thumb
 
-            success = exec(convertCmd); //kick off the command to convert movie to flv
+			tmp.delete() //delete the tmp file
+		}
+		else if (convertedMovieFileExtension == "mp4") {
 
-            if (success) success = exec(metadataCmd); //kick off the command to add the metadata
+			String convertCmd = "${mvals.ffmpeg.path} -i ${sourceVideo.absolutePath} ${mvals.ffmpeg.conversionArgs} ${targetVideo.absolutePath}"
+			String metadataCmd = "${mvals.qtfaststart.path} ${targetVideo.absolutePath} ${targetVideo.absolutePath}.1"
+			String deleteCmd = "rm -rf ${targetVideo.absolutePath}"
+			String renameCmd = "mv ${targetVideo.absolutePath}.1 ${targetVideo.absolutePath}"
+			String thumbCmd = "${mvals.ffmpeg.path} -i ${targetVideo.absolutePath} ${mvals.ffmpeg.makethumb} ${thumb.absolutePath}"
 
-            if (success) success = exec(thumbCmd); //kick off the command to create the thumb
+			success = exec(convertCmd) //kick off the command to convert movie to flv
 
-            tmp.delete()  //delete the tmp file
-        } else if (convertedMovieFileExtension == "mp4") {
+			if (success) success = exec(metadataCmd) //kick off the command to generate/manipulate the metadata
 
+			if (success) success = exec(deleteCmd) //kick off the command to generate/manipulate the metadata
+			if (success) success = exec(renameCmd) //kick off the command to generate/manipulate the metadata
 
-            String convertCmd = "${mvals.ffmpeg.path} -i ${sourceVideo.absolutePath} ${mvals.ffmpeg.conversionArgs} ${targetVideo.absolutePath}"
-            String metadataCmd = "${mvals.qtfaststart.path} ${targetVideo.absolutePath} ${targetVideo.absolutePath}.1"
-            String deleteCmd = "rm -rf ${targetVideo.absolutePath}"
-            String renameCmd = "mv ${targetVideo.absolutePath}.1 ${targetVideo.absolutePath}"
-            String thumbCmd = "${mvals.ffmpeg.path} -i ${targetVideo.absolutePath} ${mvals.ffmpeg.makethumb} ${thumb.absolutePath}"
+			if (success) success = exec(thumbCmd) //kick off the command to create the thumb
+		}
 
-            success = exec(convertCmd); //kick off the command to convert movie to flv
+		success
+	}
 
-            if (success) success = exec(metadataCmd); //kick off the command to generate/manipulate the metadata
+	@Transactional
+	void putMovie(Movie movie) {
+		movie.status = Movie.STATUS_NEW
+		movie.save()
+	}
 
-            if (success) success = exec(deleteCmd); //kick off the command to generate/manipulate the metadata
-            if (success) success = exec(renameCmd); //kick off the command to generate/manipulate the metadata
+	void deleteMovie(Movie movie) {
+		delete(movie.pathMaster)
+		delete(movie.pathThumb)
+		delete(movie.pathFlv)
+		movie.delete()
+	}
 
-            if (success) success = exec(thumbCmd); //kick off the command to create the thumb
+	private void delete(String path) {
+		File file = new File(path)
+		if (file.exists()) file.delete()
+	}
 
-            //tmp.delete()  //delete the tmp file
-        }
+	@Transactional
+	void convertVideo(Movie movie) {
 
-        return success;
-    }
+		movie.status = Movie.STATUS_INPROGRESS
+		movie.save(flush: true)
 
-    def putMovie(Movie movie) {
+		File vid = new File(movie.pathMaster)
 
-        movie.status = Movie.STATUS_NEW
-        movie.save(flush: true)
+		//create unique file paths for assets created during conversion (flv and thumb)
+		String convertedMovieFileExtension = mvals.ffmpeg.fileExtension
+		String convertedMovieThumbnailExtension = "jpg"
+		String convertedMovieFilePath = mvals.location + movie.key + "." + convertedMovieFileExtension
+		String convertedMovieThumbnailFilePath = mvals.location + movie.key + "." + convertedMovieThumbnailExtension
 
-    }
+		File flv = new File(convertedMovieFilePath)
+		File thumb = new File(convertedMovieThumbnailFilePath)
 
-    def deleteMovie(Movie movie) {
-
-        File master = new File(movie.pathMaster)
-        if (master.exists()) master.delete()
-
-        File thumb = new File(movie.pathThumb)
-        if (thumb.exists()) thumb.delete()
-
-        File flv = new File(movie.pathFlv)
-        if (flv.exists()) flv.delete()
-
-        movie.delete()
-
-    }
-
-    def convertVideo(Movie movie) {
-
-        //set movie status to in-progress
-        movie.status = Movie.STATUS_INPROGRESS
-        movie.save(flush: true)
-
-        //create file to master file contents
-        File vid = new File(movie.pathMaster)
-
-        //create unique file paths for assets created during conversion (flv and thumb)
-        String convertedMovieFileExtension = mvals.ffmpeg.fileExtension;
-        String convertedMovieThumbnailExtension = "jpg";
-        def convertedMovieFilePath = mvals.location + movie.key + "." + convertedMovieFileExtension;
-        def convertedMovieThumbnailFilePath = mvals.location + movie.key + "." + convertedMovieThumbnailExtension;
-
-        File flv = new File(convertedMovieFilePath)
-        File thumb = new File(convertedMovieThumbnailFilePath)
-
-        //convert the file into FLV
-        performConversion(vid, flv, thumb)
-
-        //set converted params on movie and save
-
-        movie.pathFlv = convertedMovieFilePath
-        movie.pathThumb = convertedMovieThumbnailFilePath
-        movie.size = flv.length()
-        movie.contentType = mvals.ffmpeg.contentType;
-
-        movie.playTime = 0;
-        movie.createDate = new Date();
-        movie.url = "/movie/display/" + movie.id
-
-        extractVideoMetadata(movie, convertedMovieFilePath)
-
-        if (flv.exists()) {
-            movie.status = Movie.STATUS_CONVERTED
-
-        } else {
-            movie.status = Movie.STATUS_FAILED
-        }
-        movie.save(flush: true);
-
-    }
-
-
-
-    def convertNewVideo() {
-        log.info("Querying for \"" + Movie.STATUS_NEW + "\" movies.");
-        def results = Movie.findAllByStatus(Movie.STATUS_NEW)
-
-        log.info("Found " + results.size() + " movie(s) to convert");
-        //TODO: kick off coversions in parallel
-        results.each {
-            log.info("Converting movie with key " + it.key);
-            convertVideo(it)
-        }
-    }
-
-
-    def exec(String command) {
-
-        try {
-            log.info("Executing " + command)
-            def out = new StringBuilder()
-            def err = new StringBuilder()
-            def proc = command.execute();
-
-            def exitStatus = proc.waitForProcessOutput(out, err)
-            if (out) log.info "out:\n$out"
-            if (err) log.info "err:\n$err"
-
-            log.info("Process exited with status " + exitStatus);
-
-            return (exitStatus == null || exitStatus == 0);
-
-        } catch (Exception e) {
-            log.error("Error while executing command " + command, e);
-            return false;
-        }
-
-    }
-
-    def extractVideoMetadata(Movie movie, String file) {
-
-        // String command = "${mvals.ffprobe.path} -pretty -i " + file + " 2>&1 | grep \"Duration\" | cut -d ' ' -f 4 | sed s/,//"
-        String command = "${mvals.ffprobe.path} ${mvals.ffprobe.params}" + file
-
-        try {
-            log.info("Executing " + command)
-            def out = new StringBuilder()
-            def err = new StringBuilder()
-            def proc = command.execute();
-
-            def exitStatus = proc.waitForProcessOutput(out, err)
-            if (out) log.info "out:\n$out"
-            if (err) log.info "err:\n$err"
-
-            log.info("Process exited with status " + exitStatus);
-
-            if (exitStatus == null || exitStatus == 0) {
-
-
-                String originalOutput = out.append(err).toString()
-
-                def tokens = []
-                originalOutput.splitEachLine(": ,\n") { line ->
-                    List list = line.toString().tokenize(": ,")
-                    list.each { item ->
-                        tokens << item
-                    }
-                }
-
-                int i;
-                for (i = 0; i < tokens.size(); i++) {
-                    if (tokens.get(i).toString().contains("Duration")) {
-                        break;
-                    }
-                }
-
-                movie.playTime = tokens.get(i + 1).toString().toInteger() * 3600 + tokens.get(i + 2).toString().toInteger() * 60 + tokens.get(i + 3).toString().toFloat()
-                return true
-            }
-            return false
-
-        } catch (Exception e) {
-            log.error("Error while executing command " + command, e);
-            return false;
-        }
-
-    }
-
+		performConversion(vid, flv, thumb)
+
+		movie.pathFlv = convertedMovieFilePath
+		movie.pathThumb = convertedMovieThumbnailFilePath
+		movie.size = flv.length()
+		movie.contentType = mvals.ffmpeg.contentType
+
+		movie.playTime = 0
+		movie.createDate = new Date()
+		movie.url = "/movie/display/" + movie.id
+
+		extractVideoMetadata(movie, convertedMovieFilePath)
+
+		if (flv.exists()) {
+			movie.status = Movie.STATUS_CONVERTED
+		}
+		else {
+			movie.status = Movie.STATUS_FAILED
+		}
+
+		movie.save()
+	}
+
+	void convertNewVideo() {
+		log.debug "Querying for '$Movie.STATUS_NEW' movies."
+		def results = Movie.findAllByStatus(Movie.STATUS_NEW)
+
+		log.debug "Found ${results.size()} movie(s) to convert"
+
+		//TODO: kick off coversions in parallel
+		for (Movie movie in results) {
+			log.debug "Converting movie with key $movie.key"
+			convertVideo movie
+		}
+	}
+
+	boolean exec(String command) {
+		try {
+			log.debug "Executing $command"
+			def out = new StringBuilder()
+			def err = new StringBuilder()
+			def proc = command.execute()
+
+			def exitStatus = proc.waitForProcessOutput(out, err)
+			if (out) log.debug "out:\n$out"
+			if (err) log.debug "err:\n$err"
+
+			log.debug "Process exited with status $exitStatus"
+
+			return exitStatus == null || exitStatus == 0
+		}
+		catch (Exception e) {
+			log.error("Error while executing command $command", e)
+			return false
+		}
+	}
+
+	boolean extractVideoMetadata(Movie movie, String file) {
+
+		// String command = "${mvals.ffprobe.path} -pretty -i " + file + " 2>&1 | grep \"Duration\" | cut -d ' ' -f 4 | sed s/,//"
+		String command = "${mvals.ffprobe.path} ${mvals.ffprobe.params}" + file
+
+		try {
+			log.debug "Executing $command"
+			def out = new StringBuilder()
+			def err = new StringBuilder()
+			def proc = command.execute()
+
+			def exitStatus = proc.waitForProcessOutput(out, err)
+			if (out) log.debug "out:\n$out"
+			if (err) log.debug "err:\n$err"
+
+			log.debug "Process exited with status $exitStatus"
+
+			if (exitStatus == null || exitStatus == 0) {
+				String originalOutput = out.append(err).toString()
+
+				def tokens = []
+				originalOutput.splitEachLine(": ,\n") { line ->
+					List list = line.toString().tokenize(": ,")
+					list.each { item -> tokens << item }
+				}
+
+				int i
+				int count = tokens.size()
+				for (i = 0; i < count; i++) {
+					if (tokens[i].toString().contains("Duration")) {
+						break
+					}
+				}
+
+				movie.playTime = tokens[i + 1].toString().toInteger() * 3600 + tokens[i + 2].toString().toInteger() * 60 + tokens[i + 3].toString().toFloat()
+				return true
+			}
+			return false
+		}
+		catch (Exception e) {
+			log.error("Error while executing command $command", e)
+			return false
+		}
+	}
 }
