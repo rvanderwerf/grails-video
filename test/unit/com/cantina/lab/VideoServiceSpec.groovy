69fc8c0c4557ca1ba5bdc9794bc1eb3b7316709e
Peter N. Steinmetz <PeterNSteinmetz@steinmetz.org>
2013-02-11 18:08:36 -0700

    Initial test of VideoService.

---
+++ test/unit/com/cantina/lab/VideoServiceSpec.groovy
@@ -0,0 +1,39 @@
+package com.cantina.lab
+
+import grails.test.mixin.domain.DomainClassUnitTestMixin
+import grails.test.mixin.services.ServiceUnitTestMixin
+import grails.test.mixin.TestFor
+import grails.test.mixin.Mock
+import spock.lang.Specification
+
+/**
+ * Tests of VideoService.
+ *
+ * These depend on the video configuration variables being set properly.
+ *
+ * @author Peter N. Steinmetz
+ */
+
+@TestFor(VideoService)
+@Mock(Movie)
+class VideoServiceSpec extends Specification {
+	
+	def testInputFile = new File("test/integration/resources/shortSamp.mp4")
+	
+	def setup() {
+		service.videoConversionService = mockService(VideoConversionService)
+		service.videoConversionService.afterPropertiesSet()
+		service.afterPropertiesSet()
+	}
+
+	def "test creation of new movie with conversion"() {
+		setup:
+		Movie mov = new Movie()
+		mov.newFile(testInputFile)
+		
+		expect:
+		service.convertVideo(mov)
+		def foundMovies = Movie.findAllByStatus("converted")
+		foundMovies.size() == 1
+	}
+}
