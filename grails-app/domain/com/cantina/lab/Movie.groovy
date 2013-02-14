4e10d8a482f86eb090dc36e90ebdbcc602f78efb
Peter N. Steinmetz <PeterNSteinmetz@steinmetz.org>
2013-02-14 11:12:57 -0700

    Update comment about converted file.

--- grails-app/domain/com/cantina/lab/Movie.groovy
+++ grails-app/domain/com/cantina/lab/Movie.groovy
@@ -47,7 +47,7 @@ class Movie {
 	Long size                  //size of content in bytes
 	Date createDate            //date the content was created
 	String createdBy           //the name of the person who created the content
-	String pathFlv             //path to the flv file
+	String pathFlv             //path to the converted file
 	String pathThumb           //the path to the thumb file - jpg
 	String pathMaster          //path to the digital master movie - i.e, mpeg, mov, etc...
 	String contentType          //the content type of the movie - usually flv
