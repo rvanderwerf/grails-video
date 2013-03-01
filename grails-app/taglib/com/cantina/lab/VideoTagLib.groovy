32a6a718d983ab7bade877a7786d0502eaf4ac2e
Peter N. Steinmetz <PeterNSteinmetz@steinmetz.org>
2013-03-01 10:09:41 -0700

    Add controls to video element so works in Safari and Chrome, though not in Firefox.

--- grails-app/taglib/com/cantina/lab/VideoTagLib.groovy
+++ grails-app/taglib/com/cantina/lab/VideoTagLib.groovy
@@ -139,14 +139,14 @@ class VideoTagLib {
 			if (stream == 'true') {
 				out << """\
                 <div class="flowplayer" data-debug="true">
-                  <video src="${g.createLink(controller: 'movie', action: 'streamMp4', id: movie.id)}"></video>
+                  <video src="${g.createLink(controller: 'movie', action: 'streamMp4', id: movie.id)}" controls></video>
                 </div>
 """
 			}
 			else {
         out << """\
                 <div class="flowplayer" data-debug="true">
-                  <video src="${g.createLink(controller: 'movie', action: 'streamMp4', id: movie.id)}"></video>
+                  <video src="${g.createLink(controller: 'movie', action: 'streamMp4', id: movie.id)}" controls></video>
                 </div>
 """
 			}
