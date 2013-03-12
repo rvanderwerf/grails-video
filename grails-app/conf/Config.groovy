18bec3d2eabbfd0b6b51aa56f0a6b5e958436d5c
Peter N. Steinmetz <PeterNSteinmetz@steinmetz.org>
2013-03-12 10:31:58 -0700

    Config settings for Mac.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -10,36 +10,30 @@ log4j = {
 }
 
 video {
-
-    location="/tmp/"
-    player.height = '260'
-    player.width =  '320'
-    yamdi {
-        path="/usr/bin/yamdi"
-    }
-
-    ffmpeg {
-        fileExtension = "mp4"
-        //fileExtension = "flv"
-
-        //conversionArgs = "-b 600k -r 24 -ar 22050 -ab 96k"
-        conversionArgs = "-b 600k -r 24 -ar 44100 -ab 128k"
-        path="/usr/bin/ffmpeg"
-        makethumb = "-an -ss 00:00:03 -an -r 2 -vframes 1 -y -f mjpeg"
-    }
-    ffprobe {
-        path="/usr/bin/ffprobe"
-        params=""
-    }
-    flowplayer {
-        version = "3.1.2"
-    }
-    swfobject {
-        version = ""
-    }
-    qtfaststart {
-        path = "/usr/local/bin/qtfaststart"
-    }
+	location="/tmp/"  // location for storage of videos, can be on a shared drive
+	yamdi{
+		path="/usr/local/bin/yamdi"    // FLV metadata injector (IF TYPE= FLV)
+	}
+	ffmpeg  {
+		fileExtension = "mp4"  // use flv or mp4
+		// conversion args should not include an output format
+		conversionArgs = "-b 600k -r 24 -ar 22050 -ab 96k"
+		path="/usr/local/bin/ffmpeg"
+		makethumb = "-an -ss 00:00:03 -an -r 2 -vframes 1 -y -f mjpeg"
+	}
+	ffprobe {
+		path="/usr/local/bin/ffprobe" // finds length of movie
+		params=""
+	}
+	flowplayer {
+		version = "3.1.2" // use empty string for no version on file
+	}
+	swfobject {
+		version = "" // used for jw-flv player, empty to not specify version
+	}
+	qtfaststart {
+		path = "/usr/local/bin/qt-faststart" // if type == mp4 used to rearrange metadata
+	}
 }
 
 // if you install this into several apps on the same database and only want some to process video, you can turn off here
