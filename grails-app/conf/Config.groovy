2982c87b75a0a17425c1c6a200f124d18c30be2c
Peter N. Steinmetz <PeterNSteinmetz@steinmetz.org>
2013-03-22 09:28:52 -0700

    Add switch for development system settings. Clean up some comments.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -10,36 +10,56 @@ log4j = {
 }
 
 video {
+  player.height = '260'
+  player.width =  '320'
+  flowplayer {
+    version = "3.1.2"
+  }
+  swfobject {
+    version = ""
+  }
 
+  // Settings for two possible systems as used in development.
+  // MacOS X with ffmpeg and yamdi installed via brew command
+  if (System.properties.getProperty("os.name")=="Mac OS X") {
     location="/tmp/"
-    player.height = '260'
-    player.width =  '320'
     yamdi {
-        path="/usr/bin/yamdi"
+      path="/usr/local/bin/yamdi"
     }
-
     ffmpeg {
-        fileExtension = "mp4"
-        //fileExtension = "flv"
-
-        //conversionArgs = "-b 600k -r 24 -ar 22050 -ab 96k"
-        conversionArgs = "-b 600k -r 24 -ar 44100 -ab 128k"
-        path="/usr/bin/ffmpeg"
-        makethumb = "-an -ss 00:00:03 -an -r 2 -vframes 1 -y -f mjpeg"
+      fileExtension = "mp4"  // should be mp4 or flv
+      conversionArgs = "-b 600k -r 24 -ar 44100 -ab 128k"
+      path="/usr/local/bin/ffmpeg"
+      makethumb = "-an -ss 00:00:03 -an -r 2 -vframes 1 -y -f mjpeg"
     }
     ffprobe {
-        path="/usr/bin/ffprobe"
-        params=""
-    }
-    flowplayer {
-        version = "3.1.2"
-    }
-    swfobject {
-        version = ""
+      path="/usr/local/bin/ffprobe"
+      params=""
     }
     qtfaststart {
-        path = "/usr/bin/qt-faststart"
+      path = "/usr/local/bin/qt-faststart"
     }
+  }
+  // other systems
+  else {
+    location="/tmp/"
+    yamdi {
+      path="/usr/bin/yamdi"
+    }
+    ffmpeg {
+      fileExtension = "mp4"
+      conversionArgs = "-b 600k -r 24 -ar 44100 -ab 128k"
+      path="/usr/bin/ffmpeg"
+      makethumb = "-an -ss 00:00:03 -an -r 2 -vframes 1 -y -f mjpeg"
+    }
+    ffprobe {
+      path="/usr/bin/ffprobe"
+      params=""
+    }
+    qtfaststart {
+      path = "/usr/bin/qt-faststart"
+    }
+  }
 }
 
 // if you install this into several apps on the same database and only want some to process video, you can turn off here
